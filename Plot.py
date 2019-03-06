814aaaf69bf8f02d169d5cfd7f99428a9ebf37c5
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-06 22:31:57 +0800

    2019.03.06.2231

--- Plot.py
+++ Plot.py
@@ -21,11 +21,22 @@ def rgbline(ax, k, e, red, green, blue, alpha=1.):
     lc = LineCollection(seg, colors=list(zip(r, g, b, a)), linewidth=2)
     ax.add_collection(lc)
 
-def load_variable(filename):
-    f = open(filename, 'rb')
-    pickle.load(f)
 
 if __name__ == "__main__":
     # 读取数据,需手动输入数据名称
-    pbands_atom = load_variable('pbands_atom.txt')
+    pbands = pickle.load(open('PBands_Mg.txt', 'rb'))
+    # 开始画图
     labels = [r"$L$", r"$\Gamma$", r"$X$", r"$U,K$", r"$\Gamma$"]
+    # 颜色变化程度
+    atom_name = 'Mg'  # 手动输入画图的原子
+    contrib = np.zeros((bands.nb_bands, len(bands.kpoints), 3))
+    for b in range(bands.nb_bands):
+        for k in range(len(bands.kpoints)):
+            sc = pbands[Spin.up][b][k][atom_name]["s"] ** 2
+            pc = pbands[Spin.up][b][k][atom_name]["p"] ** 2
+            dc = pbands[Spin.up][b][k][atom_name]["d"] ** 2
+            tot = sc + pc + dc
+            if tot != 0.0:
+                contrib[atom_name, b, k, 0] = sc / tot
+                contrib[atom_name, b, k, 1] = pc / tot
+                contrib[atom_name, b, k, 2] = dc / tot
