22875e59ce74dae0871d9ea701b24181e68e0b3f
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-06 09:45:47 +0800

    tmp_atom→orbit_atom

--- Band_Projected_Atom_Orbit.py
+++ Band_Projected_Atom_Orbit.py
@@ -67,35 +67,35 @@ if __name__ == "__main__":
     # Band Diagram
     # ------------
     name = {}
-    tmp_atom = sorted(set(run.atomic_symbols), key=run.atomic_symbols.index)
-    for i in tmp_atom:
+    orbit_atom = sorted(set(run.atomic_symbols), key=run.atomic_symbols.index)
+    for i in orbit_atom:
         name[i] = ["s", "p", "d"]
     pbands = bands.get_projections_on_elements_and_orbitals(name)
 
     # compute s, p, d normalized contributions
-    contrib = np.zeros((len(tmp_atom), bands.nb_bands, len(bands.kpoints), 3))
+    contrib = np.zeros((len(orbit_atom), bands.nb_bands, len(bands.kpoints), 3))
     for b in range(bands.nb_bands):
         for k in range(len(bands.kpoints)):
-            for atom_name in tmp_atom:
+            for atom_name in orbit_atom:
                 sc = pbands[Spin.up][b][k][atom_name]["s"] ** 2
                 pc = pbands[Spin.up][b][k][atom_name]["p"] ** 2
                 dc = pbands[Spin.up][b][k][atom_name]["d"] ** 2
                 tot = sc + pc + dc
                 if tot != 0.0:
-                    contrib[tmp_atom.index(atom_name), b, k, 0] = sc / tot
-                    contrib[tmp_atom.index(atom_name), b, k, 1] = pc / tot
-                    contrib[tmp_atom.index(atom_name), b, k, 2] = dc / tot
+                    contrib[orbit_atom.index(atom_name), b, k, 0] = sc / tot
+                    contrib[orbit_atom.index(atom_name), b, k, 1] = pc / tot
+                    contrib[orbit_atom.index(atom_name), b, k, 2] = dc / tot
 
     # plot bands using rgb mapping
     rgbline_atom = {}
     for b in range(bands.nb_bands):
-        for atom_name in tmp_atom:
+        for atom_name in orbit_atom:
             rgbline(ax1,
                     range(len(bands.kpoints)),
                     [e - bands.efermi for e in bands.bands[Spin.up][b]],
-                    contrib[tmp_atom.index(atom_name), b, :, 0],
-                    contrib[tmp_atom.index(atom_name), b, :, 1],
-                    contrib[tmp_atom.index(atom_name), b, :, 2])
+                    contrib[orbit_atom.index(atom_name), b, :, 0],
+                    contrib[orbit_atom.index(atom_name), b, :, 1],
+                    contrib[orbit_atom.index(atom_name), b, :, 2])
             rgbline_atom[atom_name] = ax1
 
     # style
