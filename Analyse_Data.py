7d75fa831aa9aa6cd26219c35606dd9aa29092f4
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-06 20:12:26 +0800

    fuck

--- Analyse_Data.py
+++ Analyse_Data.py
@@ -2,10 +2,31 @@
 # -*- coding=utf-8 -*-
 
 import copy
+import pickle
 
 import numpy as np
 from pymatgen.electronic_structure.core import Spin
 from pymatgen.io.vasp.outputs import Vasprun
+
+
+# 生成不同原子的Projected_Bands
+def Pbands_Separate(i):
+    pbands_tmp = copy.deepcopy(pbands)
+    for b in range(bands.nb_bands):
+        for k in range(len(bands.kpoints)):
+            for n in pbands[Spin.up][b][k].keys():
+                if n != i:
+                    del pbands_tmp[Spin.up][b][k][n]
+    return pbands_tmp
+
+
+# 变量保存
+def save_variable(Vars, filename):
+    f = open(filename, 'wb')
+    pickle.dump(Vars, f)
+    f.close()
+    return filename
+
 
 if __name__ == "__main__":
     # Only Bands
@@ -32,18 +53,10 @@ if __name__ == "__main__":
                     contrib[orbit_atom.index(atom_name), b, k, 1] = pc / tot
                     contrib[orbit_atom.index(atom_name), b, k, 2] = dc / tot
 
-
-    # 生成不同原子的Projected_Bands
-    def pbands_atom(i):
-        pbands_tmp = copy.deepcopy(pbands)
-        for b in range(bands.nb_bands):
-            for k in range(len(bands.kpoints)):
-                for n in pbands[Spin.up][b][k].keys():
-                    if n != i:
-                        del pbands_tmp[Spin.up][b][k][n]
-        return pbands_tmp
-
-
-    # 动态生成相应原子的名称
+    # 动态生成相应原子的名称，以及画图所需数据
+    pbands_atom = {}
     for i in orbit_atom:
-        locals()['pbands_' + i] = pbands_atom(i)
+        pbands_atom[i] = Pbands_Separate(i)
+
+
+
