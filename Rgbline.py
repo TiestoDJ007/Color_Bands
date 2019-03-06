09f434721edcf29c402a21f6d7d5ec8a0bea070f
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-06 17:48:06 +0800

    initial rgbline commit

---
+++ Rgbline.py
@@ -0,0 +1,13 @@
+def rgbline(ax, k, e, red, green, blue, alpha=1.):
+    # creation of segments based on
+    # http://nbviewer.ipython.org/urls/raw.github.com/dpsanders/matplotlib-examples/master/colorline.ipynb
+    pts = np.array([k, e]).T.reshape(-1, 1, 2)
+    seg = np.concatenate([pts[:-1], pts[1:]], axis=1)
+
+    nseg = len(k) - 1
+    r = [0.5 * (red[i] + red[i + 1]) for i in range(nseg)]
+    g = [0.5 * (green[i] + green[i + 1]) for i in range(nseg)]
+    b = [0.5 * (blue[i] + blue[i + 1]) for i in range(nseg)]
+    a = np.ones(nseg, np.float) * alpha
+    lc = LineCollection(seg, colors=list(zip(r, g, b, a)), linewidth=2)
+    ax.add_collection(lc)
