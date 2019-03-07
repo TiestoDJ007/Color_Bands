2359639f29e03a6609ffab5f070d9eccad5a6c34
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-07 20:36:46 +0800

    2019.03.07.2036

--- Analyse_Data.py
+++ Analyse_Data.py
@@ -17,6 +17,21 @@ def save_variable(Vars, filename):
     return filename
 
 
+def rgbline(ax, k, e, red, green, blue, alpha=1.):
+    # creation of segments based on
+    # http://nbviewer.ipython.org/urls/raw.github.com/dpsanders/matplotlib-examples/master/colorline.ipynb
+    pts = np.array([k, e]).T.reshape(-1, 1, 2)
+    seg = np.concatenate([pts[:-1], pts[1:]], axis=1)
+
+    nseg = len(k) - 1
+    r = [0.5 * (red[i] + red[i + 1]) for i in range(nseg)]
+    g = [0.5 * (green[i] + green[i + 1]) for i in range(nseg)]
+    b = [0.5 * (blue[i] + blue[i + 1]) for i in range(nseg)]
+    a = np.ones(nseg, np.float) * alpha
+    lc = LineCollection(seg, colors=list(zip(r, g, b, a)), linewidth=2)
+    ax.add_collection(lc)
+
+
 if __name__ == "__main__":
     # Only Bands
     vasprun = Vasprun("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/Bands/vasprun.xml",
@@ -28,11 +43,21 @@ if __name__ == "__main__":
     tmp_pbands = list(bands.projections.values())
     pbands_ndarray = deepcopy(tmp_pbands[0])
     # 分别投影到原子和轨道
-    pbands_s_Mg = np.sum(deepcopy(pbands_ndarray[:, :, 0, 0:8]), axis=3)
-    pbands_p_Mg = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 1:4, 0:8]), axis=2), axis=3)
-    pbands_d_Mg = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 4:9, 0:8]), axis=2), axis=3)
+    pbands_s_Mg = np.sum(deepcopy(pbands_ndarray[:, :, 0, 0:8]), axis=2)
+    pbands_p_Mg = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 1:4, 0:8]), axis=2), axis=2)
+    pbands_d_Mg = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 4:9, 0:8]), axis=2), axis=2)
     pbands_s_C = np.sum(deepcopy(pbands_ndarray[:, :, 0, 8:30]), axis=2)
-    pbands_p_C = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 1:4, 8:30]), axis=2), axis=3)
-    pbands_d_C = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 4:9, 8:30]), axis=2), axis=3)
+    pbands_p_C = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 1:4, 8:30]), axis=2), axis=2)
+    pbands_d_C = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 4:9, 8:30]), axis=2), axis=2)
+    total = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, :, :]), axis=2), axis=2)
 
-    # 最后需要手动输出文件，详情请见变量保存
+    # 输出投影原子，轨道的贡献率
+    contrib_Origin = np.zeros((bands.nb_bands, len(bands.kpoints), 3))
+    contrib_Mg = deepcopy(contrib_Origin)
+    contrib_Mg[:, :, 0] = (pbands_s_Mg ** 2) / total
+    contrib_Mg[:, :, 1] = (pbands_p_Mg ** 2) / total
+    contrib_Mg[:, :, 2] = (pbands_d_Mg ** 2) / total
+    contrib_C = deepcopy(contrib_Origin)
+    contrib_Mg[:, :, 0] = (pbands_s_Mg ** 2) / total
+    contrib_Mg[:, :, 1] = (pbands_p_Mg ** 2) / total
+    contrib_Mg[:, :, 2] = (pbands_d_Mg ** 2) / total
