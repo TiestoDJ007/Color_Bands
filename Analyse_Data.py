969d74803691fb0d015003367f1368bfba6f6c3f
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-07 19:29:29 +0800

    2019.03.07.1929

--- Analyse_Data.py
+++ Analyse_Data.py
@@ -2,19 +2,8 @@
 # -*- coding=utf-8 -*-
 
 import pickle
-from collections import defaultdict
 
-from pymatgen.electronic_structure.core import Spin
 from pymatgen.io.vasp.outputs import Vasprun
-
-
-# 生成不同原子的Projected_Bands
-def Pbands_Separate(i):
-    Pbands_tmp = defaultdict(dict)
-    for b in range(bands.nb_bands):
-        for k in range(len(bands.kpoints)):
-            Pbands_tmp[b][k] = pbands[Spin.up][b][k][i]
-    return Pbands_tmp
 
 
 # 变量保存
@@ -32,11 +21,7 @@ if __name__ == "__main__":
     bands = vasprun.get_band_structure("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/Bands/KPOINTS",
                                        line_mode=True,
                                        efermi=vasprun.efermi)
-    name = {}
-    orbit_atom = sorted(set(vasprun.atomic_symbols), key=vasprun.atomic_symbols.index)
-    for i in orbit_atom:
-        name[i] = ["s", "p", "d"]
-    pbands = bands.get_projections_on_elements_and_orbitals(name)
+
 
     # 动态生成相应原子的名称，以及画图所需数据
     PBands_Mg = Pbands_Separate('Mg')
