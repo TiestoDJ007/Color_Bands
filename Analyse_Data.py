9d1be9fcf54628353e5fad261dbdfe6e18d90e8b
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-07 19:56:48 +0800

    2019.03.07.1956

--- Analyse_Data.py
+++ Analyse_Data.py
@@ -2,7 +2,9 @@
 # -*- coding=utf-8 -*-
 
 import pickle
+from copy import deepcopy
 
+import numpy as np
 from pymatgen.io.vasp.outputs import Vasprun
 
 
@@ -21,11 +23,15 @@ if __name__ == "__main__":
     bands = vasprun.get_band_structure("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/Bands/KPOINTS",
                                        line_mode=True,
                                        efermi=vasprun.efermi)
-
-
-    # 动态生成相应原子的名称，以及画图所需数据
-    PBands_Mg = Pbands_Separate('Mg')
-    save_variable(PBands_Mg, 'PBands_Mg.txt')
-    save_variable(bands,'bands.txt')
+    # 将bands.projections转化成n维数组
+    tmp_pbands = list(bands.projections.values())
+    pbands_ndarray = deepcopy(tmp_pbands[0])
+    # 分别投影到原子和轨道
+    pbands_s_Mg = np.sum(deepcopy(pbands_ndarray[:, :, 0, 0:8]), axis=3)
+    pbands_p_Mg = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 1:4, 0:8]), axis=2), axis=3)
+    pbands_d_Mg = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 4:9, 0:8]), axis=2), axis=3)
+    pbands_s_C = np.sum(deepcopy(pbands_ndarray[:, :, 0, 8:30]), axis=2)
+    pbands_p_C = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 1:4, 8:30]), axis=2), axis=3)
+    pbands_d_C = np.sum(np.sum(deepcopy(pbands_ndarray[:, :, 4:9, 8:30]), axis=2), axis=3)
 
     # 最后需要手动输出文件，详情请见变量保存
