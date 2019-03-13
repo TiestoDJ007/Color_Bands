6e9c2527e0b6b87f936d40975f440387a16ba5fb
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-13 10:51:17 +0800

    添加标题

--- PBANDS_p_C_DOS.py
+++ PBANDS_p_C_DOS.py
@@ -1,62 +1,18 @@
-import sys
-from copy import deepcopy
-
-import matplotlib.pyplot as plt
 import numpy as np
 from pymatgen.electronic_structure.core import Spin  # 引入Spin函数，使之能被索引
-from pymatgen.io.vasp.outputs import Vasprun
+from pymatgen.io.vasp.outputs import Vasprun, Procar
 
 if __name__ == "__main__":
-    # Only Bands
     vasprun = Vasprun("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/Bands/vasprun.xml",
                       parse_projected_eigen=True)
     bands = vasprun.get_band_structure("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/Bands/KPOINTS",
-                                       line_mode=True,
-                                       efermi=vasprun.efermi)
-    dosrun = Vasprun("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/DOS/vasprun.xml")
-    # 将bands.projections转化成n维数组
-    tmp_pbands = list(bands.projections.values())
-    pbands_ndarray = deepcopy(tmp_pbands[0])
-    # 分别投影到原子和轨道
-    pbands_py_C = np.sum(deepcopy(pbands_ndarray[:, :, 1, 8:30]), axis=2)
-    pbands_px_C = np.sum(deepcopy(pbands_ndarray[:, :, 2, 8:30]), axis=2)
-    pbands_pz_C = np.sum(deepcopy(pbands_ndarray[:, :, 3, 8:30]), axis=2)
+                                       line_mode=True, efermi=vasprun.efermi)
+    procar = Procar("C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Color_Bands/Bands/PROCAR")
 
-    # 输出投影原子，轨道的贡献率
-    contrib_p_C = np.zeros((bands.nb_bands, len(bands.kpoints), 3))
-    for b in range(bands.nb_bands):
+    dot_size = np.zeros((((20,bands.nb_bands,len(bands.kpoints),3))))
+    for n in range(bands.nb_bands):
         for k in range(len(bands.kpoints)):
-            px = pbands_px_C[b][k] ** 2
-            py = pbands_py_C[b][k] ** 2
-            pz = pbands_pz_C[b][k] ** 2
-            tot_p = px + py + pz
-            if tot_p != 0.0:
-                contrib_p_C[b, k, 0] = px / tot_p
-                contrib_p_C[b, k, 1] = py / tot_p
-                contrib_p_C[b, k, 2] = pz / tot_p
-
-    # 设置绘图参数
-    labels = [r"$M$", r"$\Gamma$", r"$K$", r"$M$"]
-    font = {'family': 'sans-serif', 'size': 24}
-    fig, ax1 = plt.subplots()
-    ax1.set_ylim(-1,1)
-    for b in range(bands.nb_bands):
-        rgbline(ax1,
-                range(len(bands.kpoints)),
-                [e - bands.efermi for e in bands.bands[Spin.up][b]],
-                contrib_p_C[b, :, 0],
-                contrib_p_C[b, :, 1],
-                contrib_p_C[b, :, 2])
-    ax1.set_xlabel("k-points")
-    ax1.set_ylabel(r"$E - E_f$   /   eV")
-    ax1.grid()
-    ax1.hlines(y=0, xmin=0, xmax=len(bands.kpoints), color="k", lw=2)
-    nlabs = len(labels)
-    step = len(bands.kpoints) / (nlabs - 1)
-    for i, lab in enumerate(labels):
-        ax1.vlines(i * step, -1, 1, "k")
-    ax1.set_xticks([i * step for i in range(nlabs)])
-    ax1.set_xticklabels(labels)
-    ax1.set_xlim(0, len(bands.kpoints))
-    plt.show()
-    plt.savefig(sys.argv[0].strip(".py") + ".png", format="png")
+            for j in range(20,30,1):
+                dot_size[j-20][n][k][0]=procar.data[Spin.up][k][n][j][2]
+                dot_size[j-20][n][k][1]=procar.data[Spin.up][k][n][j][1]
+                dot_size[j-20][n][k][2]=procar.data[Spin.up][k][n][j][3]
---
+++ k-mesh.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+G_x = 0.0
+G_y = 0.0
+M_x = 0.33
+M_y = 0.33
+K_x = 0.5
+K_y = 0.0
+step = 0.01
+
+data_array = np.zeros(shape=(51))
