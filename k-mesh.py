09b2eaed56f3215bde9a8800112b775365109057
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-13 15:52:43 +0800

    Finial Commit

--- k-mesh.py
+++ k-mesh.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import numpy as np
 
 
@@ -39,6 +38,18 @@ if __name__ == "__main__":
             k_mesh_part_1_list.append([float(x_1), float(y_1)])
     k_mesh_part_1 = np.array(k_mesh_part_1_list)
     k_mesh_part = np.concatenate((k_mesh_part_0, k_mesh_part_1), axis=0)
-    k_mesh_part_3 = np.zeros(shape=(int(k_mesh_part.size/2),1))
-    k_mesh_part_4 = np.ones(shape=(int(k_mesh_part.size/2),1))
-    k_mesh = np.hstack((np.hstack((k_mesh_part,k_mesh_part_3)),k_mesh_part_4))
+    k_mesh_part_3 = np.zeros(shape=(int(k_mesh_part.size / 2), 1))
+    k_mesh_part_4 = np.ones(shape=(int(k_mesh_part.size / 2), 1))
+    k_mesh = np.hstack((np.hstack((k_mesh_part, k_mesh_part_3)), k_mesh_part_4))
+
+    out_file = open('KPOINTS', 'w')
+    out_file.write("k-points for fermi-surface.\n")
+    out_file.write("%d\n" % (k_mesh.shape[0]))
+    out_file.write("Reciprocal\n")
+    for i in range(np.shape(k_mesh)[0]):
+        out_file.write("{0:4f} {1:4f} {2:4f} {3:0f}\n".format(float(k_mesh[i, 0]),
+                                                              float(k_mesh[i, 1]),
+                                                              float(k_mesh[i, 2]),
+                                                              float(k_mesh[i, 3])))
+    out_file.close()
+    print("Done.")
