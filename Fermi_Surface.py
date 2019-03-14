3ad2b6b3e32a08288fc76e6e5e01d450a7b03bc5
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-14 11:24:52 +0800

    RTM version

--- Fermi_Surface.py
+++ Fermi_Surface.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
+from math import sqrt
+
 import numpy as np
 from pymatgen.electronic_structure.core import Spin
 from pymatgen.io.vasp.outputs import Vasprun
@@ -9,14 +11,36 @@ vasprun = Vasprun('C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Fermi_Surf
 bands = vasprun.get_band_structure('C:/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Fermi_Surface/KPOINTS',
                                    efermi=vasprun.efermi,
                                    line_mode=True)
-kpoints = np.zeros((len(bands.kpoints), 3))
+position = np.zeros(shape=(len(bands.kpoints), 2))
+position_original = np.zeros(shape=(len(bands.kpoints), 2, 1))
+Tr_Matrix = np.array([[1., -sqrt(3) / 3], [0, 2 * sqrt(3) / 3]])
 for i in range(len(bands.kpoints)):
-    kpoints[i][0] = bands.kpoints[i].a
-    kpoints[i][1] = bands.kpoints[i].b
+    position_original[i][0] = bands.kpoints[i].a
+    position_original[i][1] = bands.kpoints[i].b
+    temp_0 = np.dot(Tr_Matrix, position_original[i])
+    position[i] = temp_0.T
 
-emin = 0
-emax = 0
-for i in range(bands.nb_bands):
-    for j in range(len(bands.kpoints)):
-        energy_temp = bands.bands[Spin.up][i][j] - bands.efermi
-        if emin<= energy_temp and energy_temp<= emax:
+emin = -0.01
+emax = 0.01
+energy = np.zeros(shape=(len(bands.kpoints)))
+for i in range(len(bands.kpoints)):
+    for nb in range(bands.nb_bands):
+        energy_temp = bands.bands[Spin.up][nb][i] - bands.efermi
+        if emin <= energy_temp and emax >= energy_temp:
+            energy[i] = 1.
+            break
+        else:
+            energy[i] = 0.
+
+delet_list = []
+for i in range(len(bands.kpoints)):
+    if energy[i] == 0:
+        delet_list.append(i)
+
+position = np.delete(position, delet_list, axis=0)
+
+out_file = open('Fermi_Surface.dat', 'w')
+for i in range(position.shape[0]):
+    out_file.write("{0:6f} {1:6f} \n".format(position[i][0],
+                                             position[i][1]))
+out_file.close()
--- k-mesh.py
+++ k-mesh.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+
 def y(x):
     y = -2 * x + 1
     return y
@@ -9,32 +10,37 @@ def y(x):
 if __name__ == "__main__":
     G_x = 0.0
     G_y = 0.0
-    M_x = 0.33
-    M_y = 0.33
+    M_x = 0.333
+    M_y = 0.333
     K_x = 0.5
     K_y = 0.0
-    step = 0.01
+    step = 0.005
 
-    Point_x_0 = np.arange(G_x, M_x + 0.01, step)
+    Point_x_0 = np.arange(G_x, M_x + step, step)
     k_mesh_part_0_list = []
     for i_0 in range(Point_x_0.size):
-        x_0 = "%.4f" % Point_x_0[i_0]
-        c_0 = np.arange(Point_x_0[i_0], M_x + 0.01, step)
+        x_0_temp = "%.6f" % Point_x_0[i_0]
+        x_0 = float(x_0_temp)
+        c_0 = np.arange(G_x, x_0 + step, step)
         for j_0 in range(c_0.size):
-            y_0 = "%.4f" % c_0[j_0]
-            if y_0 != '0.3400':
-                k_mesh_part_0_list.append([float(x_0), float(y_0)])
+            y_0_temp = "%.6f" % c_0[j_0]
+            y_0 = float(y_0_temp)
+            if y_0 <= M_x:
+                k_mesh_part_0_list.append([x_0, y_0])
     k_mesh_part_0 = np.array(k_mesh_part_0_list)
 
-    Point_x_1 = np.arange(M_x + 0.01, K_x + 0.01, step)
+    Point_x_1 = np.arange(M_x + step, K_x + step, step)
     k_mesh_part_1_list = []
     for i_1 in range(Point_x_1.size):
-        x_1 = "%.4f" % Point_x_1[i_1]
-        f_1 = y(Point_x_1[i_1])
-        c_1 = np.arange(0, f_1 + 0.01, 0.01)
+        x_1_temp = "%.6f" % Point_x_1[i_1]
+        x_1 = float(x_1_temp)
+        y_1_max_temp = "%.6f" % y(Point_x_1[i_1])
+        y_1_max = float(y_1_max_temp)
+        c_1 = np.arange(0, y_1_max, step)
         for j in range(c_1.size):
-            y_1 = "%.4f" % c_1[j]
-            k_mesh_part_1_list.append([float(x_1), float(y_1)])
+            y_1_temp = "%.6f" % c_1[j]
+            y_1 = float(y_1_temp)
+            k_mesh_part_1_list.append([x_1, y_1])
     k_mesh_part_1 = np.array(k_mesh_part_1_list)
     k_mesh_part = np.concatenate((k_mesh_part_0, k_mesh_part_1), axis=0)
     k_mesh_part_3 = np.zeros(shape=(int(k_mesh_part.size / 2), 1))
@@ -46,9 +52,8 @@ if __name__ == "__main__":
     out_file.write("%d\n" % (k_mesh.shape[0]))
     out_file.write("Reciprocal\n")
     for i in range(np.shape(k_mesh)[0]):
-        out_file.write("{0:4f} {1:4f} {2:4f} {3:0f}\n".format(float(k_mesh[i, 0]),
+        out_file.write("{0:6f} {1:6f} {2:6f} {3:0f}\n".format(float(k_mesh[i, 0]),
                                                               float(k_mesh[i, 1]),
                                                               float(k_mesh[i, 2]),
                                                               float(k_mesh[i, 3])))
     out_file.close()
-    print("Done.")
