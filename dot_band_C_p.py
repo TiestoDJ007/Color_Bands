850e7335da67ff967e4ae618c8b927fd2f1306d0
TiestoDJ007 <jackxiao007@outlook.com>
2019-04-04 14:54:47 +0800

    RTM version

--- dot_band_C_p.py
+++ dot_band_C_p.py
@@ -9,15 +9,37 @@ if __name__ == "__main__":
     bands = vasprun.get_band_structure("/mnt/c/Users/A/OneDrive/Calculation_Data/Mg2C_Graphene/Band/KPOINTS",
                                        line_mode=True, efermi=vasprun.efermi)
     procar = Procar("/mnt/c/Users/A/OneDrive/Calculation_Data/Mg2C_Graphene/Band/PROCAR_0%")
-
-    dot_size = np.zeros((((20, bands.nb_bands, len(bands.kpoints), 3))))
+    #9-22是C原子
+    C
+    dot_size = np.zeros((((22, bands.nb_bands, len(bands.kpoints), 3))))
     for n in range(bands.nb_bands):
         for k in range(len(bands.kpoints)):
-            for j in range(20, 30, 1):
+            for j in range():
                 dot_size[j - 20][n][k][0] = procar.data[Spin.up][k][n][j][2]
                 dot_size[j - 20][n][k][1] = procar.data[Spin.up][k][n][j][1]
                 dot_size[j - 20][n][k][2] = procar.data[Spin.up][k][n][j][3]
-
+    #dot_data = np.zeros(((3,bands.nb_bands,len(bands.kpoints))))
+    #for n in range(bands.nb_bands):
+    #    for k in range(len(bands.kpoints)):
+    #        dot_data[0][b][k]=bands.bands[Spin.up][n][k]
     labels = [r"$M$", r"$\Gamma$", r"$K$", r"$M$"]
     font = {'family': 'sans-serif', 'size': 24}
     fig, ax1 = plt.subplots()
+    nlabs = len(labels)
+    step = len(bands.kpoints) / (nlabs - 1)
+    ax1.set_ylim(-1,1)
+    ax1.set_xlabel("k-points")
+    ax1.set_ylabel(r"$E - E_f$   /   eV")
+    ax1.grid()
+    ax1.hlines(y=0, xmin=0, xmax=len(bands.kpoints), color="k", lw=2)
+    nlabs = len(labels)
+    step = len(bands.kpoints) / (nlabs - 1)
+    for i, lab in enumerate(labels):
+        ax1.vlines(i * step, -1, 1, "k")
+    ax1.set_xticks([i * step for i in range(nlabs)])
+    ax1.set_xticklabels(labels)
+    ax1.set_xlim(0, len(bands.kpoints))
+    ax1.set_title('C Orbital p Projected Bands')
+    for i in range(len(bands.kpoints)):
+        band_px = ax1.scatter(bands.kpoints[i],dot_size)
+
