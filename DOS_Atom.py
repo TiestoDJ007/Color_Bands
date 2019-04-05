c3ae7d4330c4a80f711cd146a5e9aa4c00688039
TiestoDJ007 <jackxiao007@outlook.com>
2019-04-05 22:05:00 +0800

    ONE

---
+++ DOS_Atom.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+from pymatgen.io.vasp.outputs import Vasprun
+
+vasprun = Vasprun('/mnt/c/Users/jackx/OneDrive/Calculation_Data/Mg2C_Graphene/DOS/vasprun_0%.xml')
+PDOS = vasprun.pdos
+TDOS = vasprun.tdos.densities
+ENERGY_POINTS = vasprun.tdos.energies
