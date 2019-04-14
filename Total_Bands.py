cfc5df368332693b65fe481cb46d113c5de7c952
TiestoDJ007 <jackxiao007@outlook.com>
2019-04-14 17:06:48 +0800

    普通能带图

--- Total_Bands.py
+++ Total_Bands.py
@@ -2,7 +2,7 @@
 # -*- coding=utf-8 -*-
 
 import matplotlib.pyplot as plt
-
+from pymatgen.electronic_structure.core import Spin
 from pymatgen.io.vasp.outputs import Vasprun
 
 if __name__ == "__main__":
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     kpoints_file = 'KPOINTS'
     procar_file = 'PROCAR_a_-2%'
     saving_dictory = '/mnt/c/Users/a/OneDrive/Calculation_Data/Mg2C_Graphene/Picture/Band/'
-    saving_file = '{}'.format(vasprun_file.strip('vasprun_' + '.xml') + '_Band' )
+    saving_file = '{}'.format(vasprun_file.strip('vasprun_' + '.xml') + '_Band')
     vasprun = Vasprun("{}".format(vasprun_dirctory + vasprun_file))
-    band = vasprun.get_band_structure("{}".format(vasprun_dirctory+kpoints_file),
-                                       line_mode=True, efermi=vasprun.efermi)
+    bands = vasprun.get_band_structure(
+        "{}".format(vasprun_dirctory + kpoints_file),
+        line_mode=True, efermi=vasprun.efermi)
 
     energy_min = -1
     energy_max = 1
@@ -30,8 +31,8 @@ if __name__ == "__main__":
     ax1.set_ylim(energy_min, energy_max)
     # 设置x轴区间
     ax1.set_xlim(bands.distance[0], bands.distance[-1])
-    ax1.set_xlabel("k-points")
-    ax1.set_ylabel(r"$E - E_f$   /   eV")
+    ax1.set_xlabel("k-points",size=30)
+    ax1.set_ylabel(r"$E - E_f$   /   eV",size = 25)
     # 寻找高对称点
     for i in range(len(bands.distance)):
         if i == 0:
@@ -40,12 +41,18 @@ if __name__ == "__main__":
             if bands.distance[i] == bands.distance[i + 1]:
                 labels_position.append(bands.distance[i])
                 # 设置垂直线
-                ax1.vlines(bands.distance[i], energy_min, energy_max, colors='gray', linestyles='dashed')
+                ax1.vlines(bands.distance[i], energy_min, energy_max,
+                           colors='gray', linestyles='dashed',linewidth=2)
         elif i == len(bands.distance) - 1:
             labels_position.append(bands.distance[i])
     # 展示高对称点
     ax1.set_xticks(labels_position)
-    ax1.set_xticklabels(labels)
-    ax1.xaxis.set_major_locator
+    ax1.set_yticks(size=20)
+    ax1.set_xticklabels(labels,size=20)
     # 图像标题
-    ax1.set_title('C Orbital p Projected Bands')
+    title = r"$Mg_2C-Gr\/Bands$"
+    for nb in range(bands.nb_bands):
+        plt.plot(bands.distance, bands.bands[Spin.up][nb] - vasprun.efermi,
+                 color='k', linewidth=2)
+    ax1.set_title(title)
+    plt.show()
