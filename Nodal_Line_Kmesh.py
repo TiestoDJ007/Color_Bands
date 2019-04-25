6b2104842eb8a9aca0d1c21ebf13f2f732f1df6d
TiestoDJ007 <jackxiao007@outlook.com>
2019-04-25 13:49:21 +0800

    Nodal line kmesh

--- Nodal_Line_Kmesh.py
+++ Nodal_Line_Kmesh.py
@@ -1,22 +1,55 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+# !/usr/bin/env python
+
 from fractions import Fraction
 
-#定义四条边界
-def line1(x):
-    return x
+import numpy as np
 
 
-def line2(x):
-    y = x
-    return y
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
 
 
-def line3(x):
-    y = -2 * x + Fraction(1, 3)
-    return y
+class LineFunction:
+    def __init__(self, x, p1, p2):
+        self.k = Fraction(p2.gety() - p1.gety(), p2.getx() - p1.getx())
+        self.b = p1.gety() - p1.getx() * Fraction(p2.gety() - p1.gety(),
+                                                  p2.getx() - p1.getx())
+        self.x = x
+
+    def function(self):
+        y = self.k * self.x + self.b
+        return float(y)
 
 
-def line4(x):
-    y = -2 * x + Fraction(2, 3)
+if __name__ == "__main__":
+    A = Point(Fraction(1, 6), 0)
+    B = Point(Fraction(1, 3), 0)
+    C = Point(Fraction(2, 9), Fraction(2, 9))
+    D = Point(Fraction(1, 9), Fraction(1, 9))
 
+    initial_points_ls=[]
+    a= np.linspace(0, 1, 1000, endpoint=False)
+    for i in range(len(a)):
+        for j in range(len(a)):
+            initial_points_ls.append(np.array((a[i],a[j])))
+    initial_points=np.array(initial_points_ls)
+    x=initial_points[:,0]
+    y=initial_points[:,1]
+    points = []
+    for i in range(len(x)):
+        xi = x[i]
+        yi = y[i]
+        if yi >= LineFunction(xi, A, B).function() and \
+           yi >= LineFunction(xi, A, D).function() and \
+           yi <= LineFunction(xi, C, B).function() and \
+           yi <= LineFunction(xi, C, D).function():
+            points.append(np.array((xi,yi)))
